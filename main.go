0f15cc04408f2cac697c0811bfa491781d2773c8
Ian Armstrong <17armstrongian@gmail.com>
2021-07-13 12:03:54 -0400

    go.mod file and main.go changes

--- main.go
+++ main.go
@@ -3,7 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Hello, World.")
+	testName := "Ian"
+	fooValue := foo(testName)
+	fmt.Println(fooValue)
 }
 
-
+func foo(test string) string {
+	//var name string = "testing"
+	name := "testing"
+	if name == test {
+		return "They are equal"
+	}
+	return "They are not equal"
+}
