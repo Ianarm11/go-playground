387c8091d6c0c77dbf5db02399991f08b468b263
Ian Armstrong <17armstrongian@gmail.com>
2021-08-30 20:18:15 -0400

    Added HTML templates and handler functions

--- main.go
+++ main.go
@@ -2,19 +2,38 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 const portNumber = ":8080"
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hey! How's it going?")
-		if err == nil {
-			fmt.Println(w, err)
-		}
-		fmt.Println(fmt.Sprintln("Number of Bytes: ", n))
-		fmt.Printf("The Number of Bytes: %d\n", n)
-	})
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
 	_ = http.ListenAndServe(portNumber, nil)
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.page.tmpl")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.page.tmpl")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, parsingFilesErr := template.ParseFiles("../src/templates/" + tmpl)
+	if parsingFilesErr == nil {
+		executeErr := parsedTemplate.Execute(w, nil)
+		if executeErr != nil {
+			fmt.Println("Error in executing templates. Error Output: ")
+			fmt.Println(executeErr)
+			return
+		}
+	} else {
+		fmt.Println("Error in parsing templates. Error Output: ")
+		fmt.Println(parsingFilesErr)
+	}
 }
