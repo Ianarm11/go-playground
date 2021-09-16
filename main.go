7efddae6afb2757abd5baa3264b001c9de118834
Ian Armstrong <17armstrongian@gmail.com>
2021-09-16 00:41:14 -0400

    Comments

--- main.go
+++ main.go
@@ -4,30 +4,16 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 )
 
 const portNumber = ":8080"
 
 func main() {
 	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	//http.HandleFunc("/about", About)
 
 	_ = http.ListenAndServe(portNumber, nil)
-
-	/*list := data_structures.List{}
-	list.Push(2)
-	list.Push(3)
-	list.Push(1)
-	list.Push(400)
-	list.ReverseList(list.GetHead())*/
-
-	/*queue := data_structures.Queue{}
-	queue = queue.Enqueue(1)
-	queue = queue.Enqueue(3)
-	queue = queue.Enqueue(60)
-	queue = queue.Dequeue()
-	queue = queue.Enqueue(4)
-	queue.Display()*/
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
@@ -39,7 +25,8 @@ func About(w http.ResponseWriter, r *http.Request) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, parsingFilesErr := template.ParseFiles("../src/templates/" + tmpl)
+	homeDirectory, _ := os.Getwd()
+	parsedTemplate, parsingFilesErr := template.ParseFiles(homeDirectory + "/templates/" + tmpl)
 	if parsingFilesErr == nil {
 		executeErr := parsedTemplate.Execute(w, nil)
 		if executeErr != nil {
