9e8af899fb050d84514db46b42430693d59733e8
Ian Armstrong <ian@Ians-MBP.lan>
2022-11-02 21:12:27 -0400

    Added a lot of new api calls and templates. Next up? interfaces and dynamic urls w/ query parameters

--- api/api.go
+++ api/api.go
@@ -9,43 +9,87 @@ import (
 
 func SetApiHandlers() {
 	http.HandleFunc("/getpreviews/", GetPreviews)
+	http.HandleFunc("/getpost/{id}", GetPost)
 }
 
-func DecodeData(response *http.Response) DumbPost {
+func DecodePreview(response *http.Response) Preview {
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		fmt.Println("Error in reading response body")
+		fmt.Println("Error: Not reading response body")
 	}
-	var post DumbPost
-	json.Unmarshal([]byte(body), &post)
-	return post
+	var Preview Preview
+	json.Unmarshal([]byte(body), &Preview)
+	return Preview
+}
+
+func DecodePost(response *http.Response) Post {
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Error: Not reading response body")
+	}
+	var Post Post
+	json.Unmarshal([]byte(body), &Post)
+	return Post
 }
 
 func GetPreviews(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		//Log entry point
-		fmt.Println("Hit getpreviews endpoint")
+		fmt.Println("Info: hitting GetPreviews api call")
 
 		//Make DB call to get data. Using dummy data now
-		dumbData1 := DummyPost{Title: "Bronze Age Mindset", Date: 03122022, Summary: "A journey past the bugmen's cruel world."}
+		packet := PreviewPacket{Title: "Bronze Age Mindset", Date: 03122022, Summary: "A journey past the bugmen's cruel world.", Id: "1"}
 
 		//Send out the encoded data
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(dumbData1)
+		json.NewEncoder(w).Encode(packet)
 		return
 	} else {
-		fmt.Println("For some reason its not a GET Request?")
+		fmt.Println("Error: Not a GET request in GetPreviews api call")
 	}
 }
 
-type DummyPost struct {
+func GetPost(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		//Log entry point
+		fmt.Println("Info: hitting GetPost api call")
+
+		//Make DB call to get data. Using dummy data now
+		packet := PostPacket{Title: "Bronze Age Mindset", Date: 03122022, Body: "A journey past the bugmen's cruel world.", Id: "1"}
+
+		//Send out the encoded data
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(packet)
+		return
+	} else {
+		fmt.Println("Error: Not a GET request in GetPreviews api call")
+	}
+}
+
+type PreviewPacket struct {
 	Title   string `json:"title"`
 	Date    int    `json:"date"`
 	Summary string `json:"summary"`
+	Id      string `josn:"id"`
 }
 
-type DumbPost struct {
+type PostPacket struct {
+	Title string `json:"title"`
+	Date  int    `json:"date"`
+	Body  string `json:"summary"`
+	Id    string `josn:"id"`
+}
+
+type Preview struct {
 	Title   string
 	Date    int
 	Summary string
+	Id      string
+}
+
+type Post struct {
+	Title string
+	Date  int
+	Body  string
+	Id    string
 }
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -13,30 +13,52 @@ func SetHandlers() {
 	http.HandleFunc("/home/", Home)
 	http.HandleFunc("/aboutme/", AboutMe)
 	http.HandleFunc("/posts/", Previews)
+	http.HandleFunc("/posts/{id}", Post)
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Info: hitting Home endpoint")
 	temp := template.Must(template.ParseFiles("templates/home.page.tmpl"))
 	temp.Execute(w, nil)
 }
 
 func AboutMe(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Info: hitting AboutMe endpoint")
 	temp := template.Must(template.ParseFiles("templates/aboutme.page.tmpl"))
 	temp.Execute(w, nil)
 }
 
 func Previews(w http.ResponseWriter, r *http.Request) {
-	temp := template.Must(template.ParseFiles("templates/posts.page.tmpl"))
+	fmt.Println("Info: hitting Previews endpoint")
+	temp := template.Must(template.ParseFiles("templates/preview.page.tmpl"))
 
 	//Make a GET request to receive data
 	//Will be a lists of Previews (obj) that store the title, date, and summary
 	response, err := http.Get("http://localhost:8080/getpreviews")
 	if err != nil {
-		fmt.Println("Error in GET request for Previews")
+		fmt.Println("Error: GET request in Previews endpoint")
 	}
 	defer response.Body.Close()
 
-	post := api.DecodeData(response)
-	fmt.Println(post.Title)
+	preview := api.DecodePreview(response)
+	temp.Execute(w, preview)
+}
+
+func Post(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Info: hitting Post endpoint")
+	temp := template.Must(template.ParseFiles("templates/post.page.tmpl"))
+
+	//Get the id from url
+	id := r.Header.Get("id")
+	fmt.Println("Post Id: " + id)
+
+	//Make GET request to get title, date, and body
+	response, err := http.Get("http://localhost:8080/getpost/" + id)
+	if err != nil {
+		fmt.Println("Error: GET request in Post endpoint")
+	}
+	defer response.Body.Close()
+
+	post := api.DecodePost(response)
 	temp.Execute(w, post)
 }
