70d7d7731261dd66cf1d61f2a70cdaceb9ca482e
Ianarm11 <17armstrongian@gmail.com>
2023-05-24 11:04:46 -0400

    Test commit, hopefully everything works still

--- main.go
+++ main.go
@@ -1,20 +1,23 @@
 package main
 
 import (
-	"fmt"
-	a "go-playground/go-playground/api"
-	h "go-playground/go-playground/handlers"
-	"net/http"
+	//"fmt"
+	//a "go-playground/go-playground/api"
+	//h "go-playground/go-playground/handlers"
+	//"net/http"
+	finance "go-playground/go-playground/finance"
 )
 
 const portNumber = ":8080"
 
 func main() {
-	h.SetHandlers()
+	/*h.SetHandlers()
 	a.SetApiHandlers()
 
 	err := http.ListenAndServe(portNumber, nil)
 	if err != nil {
 		fmt.Println("ListenAndServe: ", err)
-	}
+	}*/
+	finance.Finance()
+
 }
